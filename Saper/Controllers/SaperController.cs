170c37c0bd496cc98079154a4d422aa07edff1a7
Just Paweł <pawel.mlo@op.pl>
2022-04-01 00:53:17 +0200

    Better of mapping zeros

--- Saper/Controllers/SaperController.cs
+++ Saper/Controllers/SaperController.cs
@@ -3,8 +3,7 @@ using Saper.Models.SaperLogic;
 
 namespace Saper.Controllers
 {
-    public class SaperController : Controller
-    {
+    public class SaperController : Controller {
         private static AbstractGameBoard gameBoard;
         public IActionResult Index() {
             if (gameBoard != null)
@@ -14,14 +13,16 @@ namespace Saper.Controllers
             gameBoard.SetBombsToBoard();
             return View(gameBoard);
         }
-
         public IActionResult Check(int x, int y) {
-            if (gameBoard.Board[x, y] == -1)
-                return RedirectToAction("Index", "Home");
+            if (gameBoard.Board[x, y] == -1) {
+                gameBoard = null;
+                TempData["Failed"] = "Spróbuj jeszcze raz!";
+                return RedirectToAction("Index");
+            }
             else if (gameBoard.Board[x, y] == 0) {
-                 int axis = (int)Math.Sqrt(gameBoard.GetSize())-1;
+                int axis = (int)Math.Sqrt(gameBoard.GetSize()) - 1;
                 Seeking(gameBoard.Board, axis, x, y);
-
+                SeekingMinus(gameBoard.Board, axis, x, y);
             }
 
             gameBoard.ClickedXY()[x,y] = 1; // One - Clicked
@@ -29,53 +30,48 @@ namespace Saper.Controllers
             return RedirectToAction("Index");
         }
         private static int[,] Seeking(int[,] Tab,int axis, int x, int y) {
-            if ( x>= axis || y >= axis || y < 0 || x < 0)
+            if ( x> axis || y > axis || y < 0 || x < 0)
                 return Tab;
 			if (Tab[x, y] != 0) {
                 gameBoard.ClickedXY()[x, y] = 1;
                 return Tab;
             }
 
-            if(Seeking(Tab,axis, x+1,y)[x,y] == 0) {
-                gameBoard.ClickedXY()[x, y] = 1;
-                
-            }
-            if (Seeking(Tab, axis, x, y+1)[x, y] == 0) {
+            Seeking(Tab, axis, x + 1, y);
+
+
+            Seeking(Tab, axis, x, y + 1);
+
+
+
+            Seeking(Tab, axis, x + 1, y + 1);
                 gameBoard.ClickedXY()[x, y] = 1;
 
-            }
-            if (Seeking(Tab, axis, x + 1, y + 1)[x, y] == 0) {
-                gameBoard.ClickedXY()[x, y] = 1;
-
-            }
+            
            
-            // Nie mam pojęcia dlaczego przy wartosci y-1 wyskakuje jakas pętla (nie chciało od y odjąc jeden)
-            /*  if ( Seeking(Tab, axis, x + 1, y+1)[x, y] == 0) {
-                  gameBoard.ClickedXY()[x, y] = 1;
-
-              } 
-              if ( Seeking(Tab, axis, x, y+1)[x, y] == 0) {
-                  gameBoard.ClickedXY()[x, y] = 1;
-
-              }
-              if(Seeking(Tab,axis, x-1,y)[x,y] == 0) {
-                  gameBoard.ClickedXY()[x, y] = 1;
-
-              }
-
-              if ( Seeking(Tab, axis, x - 1, y - 1)[x, y] == 0) {
-                  gameBoard.ClickedXY()[x, y] = 1;
-
-              }
-              if ( Seeking(Tab, axis, x , y - 1)[x, y] == 0) {
-                  gameBoard.ClickedXY()[x, y] = 1;
-
-              }*/
-
 
             return Tab;
 		}
-      
-        
+        private static int[,] SeekingMinus(int[,] Tab, int axis, int x, int y) {
+            if (x > axis || y > axis || y < 0 || x < 0)
+                return Tab;
+
+            if (Tab[x, y] != 0) {
+                gameBoard.ClickedXY()[x, y] = 1;
+                return Tab;
+            }
+            SeekingMinus(Tab, axis, x, y - 1);
+
+
+            SeekingMinus(Tab, axis, x - 1, y);
+
+
+            SeekingMinus(Tab, axis, x - 1, y - 1);
+                  gameBoard.ClickedXY()[x, y] = 1;
+
+
+            return Tab;
+        }
+
     }
 }
